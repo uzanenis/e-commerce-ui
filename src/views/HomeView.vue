5b73ebd922af9aafc59cea86b75be920f1cc85b6
tunahan.kutlu <tunahan.kutlu@feux.digital>
2022-10-08 01:11:36 +0300

    shop list view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,15 @@
 <template>
-  <hello-world />
+  <div>
+    home
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'HomeView',
 
     components: {
-      HelloWorld,
     },
   }
 </script>
